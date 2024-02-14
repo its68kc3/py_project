4f8b1a907edfc8923002fa7268628744e0172693
Kenneth <kennethcallejo1996@gmail.com>
2024-02-14 04:46:25 -0500

    add python file

---
+++ main.py
@@ -0,0 +1,9 @@
+class main:
+    def __init__(self):
+        print('Main class initialized')
+    
+    def print(message):
+        print(message)
+
+obj = main()
+obj.print('Can I have a cup of coffee now? :((')
