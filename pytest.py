23658c0163359af1d73328f50849ecae1ee4baa7
Kenneth <kennethcallejo1996@gmail.com>
2024-02-14 04:05:46 -0500

    added python file

---
+++ pytest.py
@@ -0,0 +1,9 @@
+class test:
+    def __init__(self, message):
+        self.message = message
+    
+    def printmessage(self):
+        print(self.message)
+
+x = test('Hello, world! :)')
+x.printmessage()
